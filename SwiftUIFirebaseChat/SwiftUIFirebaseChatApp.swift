6f10777d49fb356c11c0ed70fc97f63d63576abd
oguzhanoozer <oguzoozer@gmail.com>
2024-10-09 21:46:39 +0300

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by oguzhan on 9.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
